5c4631d72fb0d396ba8b093fdb3ccf26b89a9eb3
Kelechi Ukpabi <kuolingou@gmail.com>
2024-09-12 10:48:28 -0400

    Add dropdown for filters

--- frontend/src/components/HomepageComp.jsx
+++ frontend/src/components/HomepageComp.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import FootComp from './FootComp';
 import Navbar from './Navbar';
 
-function HomepageComp(){
+function HomepageComp() {
 
   const [workouts, setWorkouts] = useState([]);
   const [workoutForm, setWorkoutForm] = useState({
@@ -12,6 +12,35 @@ function HomepageComp(){
     distance: '',
     heart_rate: ''
   });
+  const [filters, setFilters] = useState({
+    workout_name: '',
+    min_duration: '',
+    max_duration: '',
+    min_distance: '',
+    max_distance: '',
+    heart_rate: '',
+    start_date: '',
+    end_date: ''
+  });
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+
+  const toggleFilterBar = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
+  // Function that fetches the workouts based on provided filters
+  const fetchWorkouts = async () => {
+    try {
+
+      // Uses url search params api to turn filters into a search query
+      const query = new URLSearchParams(filters).toString();
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/get-workouts?${query}`);
+      setWorkouts(response.data);
+    } catch (err) {
+      console.error("Error fetching workouts:", err);
+    }
+  };
 
 
 
@@ -22,6 +51,14 @@ function HomepageComp(){
     setWorkoutForm({
       ...workoutForm,
       [name]: value
+    });
+  };
+
+  // Handling change of filters
+  const handleFilterChange = (e) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value
     });
   };
 
@@ -45,26 +82,15 @@ function HomepageComp(){
 
   // Fetch workouts after page is rendered
   useEffect(() => {
-    const fetchWorkouts = async () => {
-      try{
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/get-workouts`)
-        const formattedWorkouts = response.data;
-        setWorkouts(formattedWorkouts);
-      }
-      catch(error){
-        console.error('Error fetching workouts:', error);
-      }
-    };
-
     fetchWorkouts();
-  }, []);
+  }, [filters]);
 
 
   // Function to add a workout (used with add workout form)
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    try{
+    try {
       const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/add-workout`, workoutForm);
       console.log('Workout added:', response.data);
 
@@ -76,7 +102,7 @@ function HomepageComp(){
         heart_rate: ''
       })
     }
-    catch(err){
+    catch (err) {
       console.log("Can't add workout")
     }
   }
@@ -85,69 +111,141 @@ function HomepageComp(){
     <>
       <Navbar />
       <div className='app-container'>
-        
+
         <div className='body-container'>
+          <div className='filter-container'>
+            <div className='filter-bar' onClick={toggleFilterBar}>
+              Filter
+            </div>
+            {isFilterOpen && (
+                <div className='filter-dropdown'>
+                  <input
+                    name='workout_name'
+                    type='text'
+                    value={filters.workout_name}
+                    onChange={handleFilterChange}
+                    placeholder="Workout Name"
+                  />
+
+                  <input
+                    name='min_duration'
+                    type='text'
+                    value={filters.min_duration}
+                    onChange={handleFilterChange}
+                    placeholder="Min Duration"
+                  />
+
+                  <input
+                    name='max_duration'
+                    type='text'
+                    value={filters.max_duration}
+                    onChange={handleFilterChange}
+                    placeholder="Max Duration"
+                  />
+
+                  <input
+                    name='min_distance'
+                    type='text'
+                    value={filters.min_distance}
+                    onChange={handleFilterChange}
+                    placeholder="Min Distance"
+                  />
+
+                  <input
+                    name='max_distance'
+                    type='text'
+                    value={filters.max_distance}
+                    onChange={handleFilterChange}
+                    placeholder="Max Distance"
+                  />
+
+                  <input
+                    name='heart_rate'
+                    type='text'
+                    value={filters.heart_rate}
+                    onChange={handleFilterChange}
+                    placeholder="Heart Rate"
+                  />
+
+                  <input
+                    name='start_date'
+                    type='date'
+                    value={filters.start_date}
+                    onChange={handleFilterChange}
+                  />
+
+                  <input
+                    name='end_date'
+                    type='date'
+                    value={filters.end_date}
+                    onChange={handleFilterChange}
+                  />
+                </div>
+              )}
+          </div>
+
+
           <div className='inputs-container'>
             <div className='text-box'>
-            <form method='post' className="add-workout-form" onSubmit={handleSubmit}>
-              <label htmlFor="workout-name">Workout Name</label>
-              <input
-                name='workout_name'
-                id="workout_name"
-                placeholder='Workout Name'
-                type="text"
-                value={workoutForm.workout_name}
-                onChange={handleInputChange}
-                required
-              />
+              <form method='post' className="add-workout-form" onSubmit={handleSubmit}>
+                <label htmlFor="workout-name">Workout Name</label>
+                <input
+                  name='workout_name'
+                  id="workout_name"
+                  placeholder='Workout Name'
+                  type="text"
+                  value={workoutForm.workout_name}
+                  onChange={handleInputChange}
+                  required
+                />
 
-              <label htmlFor="duration">Duration</label>
-              <input
-                name='duration'
-                id="duration"
-                placeholder='Duration (in seconds)'
-                type="text"
-                value={workoutForm.duration}
-                onChange={handleInputChange}
-                required
-              />
+                <label htmlFor="duration">Duration</label>
+                <input
+                  name='duration'
+                  id="duration"
+                  placeholder='Duration (in seconds)'
+                  type="text"
+                  value={workoutForm.duration}
+                  onChange={handleInputChange}
+                  required
+                />
 
-              <label htmlFor="distance">Distance</label>
-              <input
-                name='distance'
-                id="distance"
-                placeholder='Distance'
-                type="text"
-                value={workoutForm.distance}
-                onChange={handleInputChange}
-                required
-              />
+                <label htmlFor="distance">Distance</label>
+                <input
+                  name='distance'
+                  id="distance"
+                  placeholder='Distance'
+                  type="text"
+                  value={workoutForm.distance}
+                  onChange={handleInputChange}
+                  required
+                />
 
-              <label htmlFor="heart-rate">Heart Rate</label>
-              <input
-                name='heart_rate'
-                id="heart_rate"
-                placeholder='Heart Rate'
-                type="text"
-                value={workoutForm.heart_rate}
-                onChange={handleInputChange}
-                required
-              />
+                <label htmlFor="heart-rate">Heart Rate</label>
+                <input
+                  name='heart_rate'
+                  id="heart_rate"
+                  placeholder='Heart Rate'
+                  type="text"
+                  value={workoutForm.heart_rate}
+                  onChange={handleInputChange}
+                  required
+                />
 
-              <button className='add-workout-button' type='submit'>Add Workout</button>
-            </form>
-              
+                <button className='add-workout-button' type='submit'>Add Workout</button>
+              </form>
+
             </div>
-            
+
           </div>
-          
+
           <div className='workouts-container'>
             <p className='workouts-container-title'>Workouts</p>
             {workouts.length > 0 ? (
               workouts.map((workout, index) => (
                 <div key={index} className='workout-item'>
                   <div className='workout-details'>
-                    <span>{workout.workout_name}</span> 
+                    <span>{workout.workout_name}</span>
                     <span title='Duration'><img className='icons' src="time-icon.svg" alt="duration icon" /> {workout.duration}s</span>
                     <span title='Distance'><img className='icons' src="distance-icon.svg" alt="distance icon" /> {workout.distance} miles</span>
                     <span title='Heart Rate'><img className='icons' src="heart-icon.svg" alt="heart icon" /> {workout.heart_rate} bpm</span>
@@ -158,9 +256,9 @@ function HomepageComp(){
             ) : (
               <p>No workouts available</p>
             )}
-            
+
           </div>
-            
+
         </div>
       </div>
       <FootComp />
